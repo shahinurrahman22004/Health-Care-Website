4ff22033fbfce456fe598797abfe586b4431689e
shahinurrahman22004 <shahinur02345@gmail.com>
2021-10-19 22:12:57 +0600

    img solve

--- src/pages/Home/Data/News/News.js
+++ src/pages/Home/Data/News/News.js
@@ -5,37 +5,37 @@ import Data from '../Data/Data';
 const Newses = [
     
     {
-        img: 'http://html.kodesolution.live/html/health-beauty/medical/medinova-html/v3.2/demo/images/blog/1.jpg',
+        img: 'https://i.ibb.co/h8btkGp/1.jpg',
         name: 'DIFFERENT TYPES OF STROKE',
         para: 'Lorem ipsum dolor sit amet, con amit sectetur adipisicing elit.'
     },
 
     {
-        img: 'http://html.kodesolution.live/html/health-beauty/medical/medinova-html/v3.2/demo/images/blog/2.jpg',
+        img: 'https://i.ibb.co/xX9bGx3/2.jpg',
         name: 'DIFFERENT TYPES OF STROKE',
         para: 'Lorem ipsum dolor sit amet, con amit sectetur adipisicing elit.'
     },
     
     {
-        img: 'http://html.kodesolution.live/html/health-beauty/medical/medinova-html/v3.2/demo/images/blog/3.jpg',
+        img: 'https://i.ibb.co/w77PBNx/3.jpg',
         name: 'DIFFERENT TYPES OF STROKE',
         para: 'Lorem ipsum dolor sit amet, con amit sectetur adipisicing elit.'
     },
 
     {
-        img: 'http://html.kodesolution.live/html/health-beauty/medical/medinova-html/v3.2/demo/images/blog/4.jpg',
+        img: 'https://i.ibb.co/D99KYqC/4.jpg',
         name: 'DIFFERENT TYPES OF STROKE',
         para: 'Lorem ipsum dolor sit amet, con amit sectetur adipisicing elit.'
     },
 
     {
-        img: 'http://html.kodesolution.live/html/health-beauty/medical/medinova-html/v3.2/demo/images/blog/5.jpg',
+        img: 'https://i.ibb.co/pj9B7hs/5.jpg',
         name: 'DIFFERENT TYPES OF STROKE',
         para: 'Lorem ipsum dolor sit amet, con amit sectetur adipisicing elit.'
     },
 
     {
-        img: 'http://html.kodesolution.live/html/health-beauty/medical/medinova-html/v3.2/demo/images/blog/6.jpg',
+        img: 'https://i.ibb.co/Bs3G2y0/6.jpg',
         name: 'DIFFERENT TYPES OF STROKE',
         para: 'Lorem ipsum dolor sit amet, con amit sectetur adipisicing elit.'
     }
--- src/pages/Home/Doctors/Doctors.js
+++ src/pages/Home/Doctors/Doctors.js
@@ -5,28 +5,28 @@ import Doctor from '../Doctor/Doctor';
 const OurDoctors = [
     
     {
-        img: 'http://html.kodesolution.live/html/health-beauty/medical/medinova-html/v3.2/demo/images/team/team1.jpg',
+        img: 'https://i.ibb.co/Xyj9TZs/team1.jpg',
         name: 'Dr. Smile Jhon',
         Profetion: 'Orthopaedics',
         para: 'Lorem ipsum dolor sit amet, con amit sectetur adipisicing elit.'
     },
 
     {
-        img: 'http://html.kodesolution.live/html/health-beauty/medical/medinova-html/v3.2/demo/images/team/team2.jpg',
+        img: 'https://i.ibb.co/Y70dzPr/team2.jpg',
         name: 'Dr. Mark Jacobson',
         Profetion: 'Cardiology',
         para: 'Lorem ipsum dolor sit amet, con amit sectetur adipisicing elit.'
     },
     
     {
-        img: 'http://html.kodesolution.live/html/health-beauty/medical/medinova-html/v3.2/demo/images/team/team3.jpg',
+        img: 'https://i.ibb.co/s2BVXYb/team3.jpg',
         name: 'Dr. Jaka Alex',
         Profetion: 'Neurology',
         para: 'Lorem ipsum dolor sit amet, con amit sectetur adipisicing elit.'
     },
 
     {
-        img: 'http://html.kodesolution.live/html/health-beauty/medical/medinova-html/v3.2/demo/images/team/team4.jpg',
+        img: 'https://i.ibb.co/rbR53P0/team4.jpg',
         name: 'Dr. Alex Davidson',
         Profetion: 'Haematology',
         para: 'Lorem ipsum dolor sit amet, con amit sectetur adipisicing elit.'
