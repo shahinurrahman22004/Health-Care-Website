5e1f56880ae4b3c996f72f74128773fee5564273
shahinurrahman22004 <shahinur02345@gmail.com>
2021-10-19 19:34:45 +0600

    netlify error

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -4,7 +4,6 @@ import useAuth from '../../hooks/useAuth';
 
 const Detail = () => {
     const {doctorId} = useParams();
-    const {name, img} = useAuth();
     return (
         <div>
             <h1>This is detail: {doctorId}</h1>
